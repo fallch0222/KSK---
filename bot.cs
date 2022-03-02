d08300aa56fd39716e9783463c39b62523bfda7a
fallch0222 <56762890+fallch0222@users.noreply.github.com>
2022-03-02 23:52:59 +0900

    1.0.5 Build 3

--- bot.cs
+++ bot.cs
@@ -2,6 +2,7 @@ using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace DiscordBot
@@ -37,14 +38,20 @@ namespace DiscordBot
             client.Log += OnClientLogReceived;    
             commands.Log += OnClientLogReceived;
 
-            await client.LoginAsync(TokenType.Bot, ""); //봇의 토큰을 사용해 서버에 로그인
+            await client.LoginAsync(TokenType.Bot, "token"); //봇의 토큰을 사용해 서버에 로그인
             await client.StartAsync();                         //봇이 이벤트를 수신하기 시작
 
             client.MessageReceived += OnClientMessage;         //봇이 메시지를 수신할 때 처리하도록 설정
+            
+           
+           
+
+            
 
             await Task.Delay(-1);   //봇이 종료되지 않도록 블로킹
         }
 
+        public bool WillItMention = true;
         private async Task OnClientMessage(SocketMessage arg)
         {
             //수신한 메시지가 사용자가 보낸 게 아닐 때 취소
@@ -55,27 +62,134 @@ namespace DiscordBot
             if (message.Author.IsBot)
                 return;
 
+            ulong channelID = Convert.ToUInt64(945660888336187442); 
+            var textChannel = (SocketTextChannel)client.GetChannel(channelID);
+            if (message.Author.Id == 371571375116386304)
+            {
+                if (message.Content.Substring(0, 1) == "!" && message.Content.Substring(1,4) == "긴급삭제")
+                {
+                    UInt64 willDel = Convert.ToUInt64(message.Content.Split(' ')[1]);
+                    
+
+                  
+
+                    await textChannel.DeleteMessageAsync(willDel);
+
+                    EmbedBuilder embedBuilder = new EmbedBuilder();
+                    embedBuilder.WithTitle("삭제 완료");
+                    embedBuilder.WithColor(Color.Red);
+                    message.Channel.SendMessageAsync(embed: embedBuilder.Build());
+
+
+                }
+            }
+            
+            if (message.Channel.Id == 877042810551947324)
+            {
+                if (message.Content.Substring(0, 1) == "!" && message.Content.Substring(1,4) == "긴급삭제")
+                {
+                    UInt64 willDel = Convert.ToUInt64(message.Content.Split(' ')[1]);
+                    
+
+                  
+
+                    await textChannel.DeleteMessageAsync(willDel);
+
+                    EmbedBuilder embedBuilder = new EmbedBuilder();
+                    embedBuilder.WithTitle("삭제 완료");
+                    embedBuilder.WithColor(Color.Red);
+                    message.Channel.SendMessageAsync(embed: embedBuilder.Build());
+
+
+                }
+            }
             Task.Delay(10);
             if (!message.Channel.Name.Contains("질문방"))
                 return;
             
-
-            
+             
             
             var context = new SocketCommandContext(client, message);                    //수신된 메시지에 대한 컨텍스트 생성
 
-            ulong channelID = Convert.ToUInt64(945660888336187442); 
-            var textChannel = (SocketTextChannel)client.GetChannel(channelID);
+           
 
             EmbedBuilder eb = new EmbedBuilder();
 
-            string fieldContent = message.Author.Mention + "님이 <#" + context.Channel.Id + ">에 " + message.Content +
-                                  "에 대한 질문을 개시하셨습니다!";
-            eb.AddField("질문 알림!", fieldContent);
+            string fieldContent = message.Author.Mention + $@"님이 <#" + context.Channel.Id + ">에 **[" + message.Content +
+                                  $"]({message.GetJumpUrl()})**에 대한 질문을 개시하셨습니다!";
+
+            string mentionContents = "none";
+            if (WillItMention)
+            {
+                mentionContents = mentionMessageContent(message.Channel.Id);
+
+                if (mentionContents != "none")
+                {
+                    WillItMention = false;
+                    mentionTimer(600000);
+                }
+                
+
+            }
             
-            textChannel.SendMessageAsync( embed: eb.Build()); 
+            eb.AddField("질문 알림!", fieldContent);
+
+            if (mentionContents != "none")
+            {
+                textChannel.SendMessageAsync(embed: eb.Build(), text:"\n||" + mentionContents + "||"); 
+            }
+
+            else
+            {
+                textChannel.SendMessageAsync(embed: eb.Build()); 
+            }
         }
 
+        public string mentionMessageContent(UInt64 ID)
+        {
+            switch (ID)
+            {
+                case 779641423502770206: //python
+                    return "<@&780316135282704395>";
+                
+                case  780296209939431424: //java
+                    return "<@&780316403814498324>";
+                
+                case 779978769493655563: //c and cpp
+                    return "<@&780281390897168416>";
+                
+                case 817250922211573791: //Html and Css
+                    return "<@&819377019342815272>";
+                
+                case 779978564723277845: //JavaScript
+                    return "<@&780316494906261525>";
+                
+                case 801735664528785418: //TypeScript
+                    return "<@&948132537371340830>";
+                
+                case 787629853692264468: //Csharp
+                    return "<@&877406923899674624>";
+                
+                case 921627644016066640: //Unity
+                    return "<@&948123651713146890>";
+                
+                case 932564429277626398: //linux
+                    return "<@&948126355365720065>";
+                
+                case 821212431568207913: //others
+                    return "none";
+                
+                default:
+                    return "ERROR!!! CH. ID NOT FOUND";
+            }
+        }
+
+        public async Task mentionTimer(int MS)
+        {
+            await Task.Delay(MS);
+            WillItMention = true;
+            return;
+        }
         /// <summary>
         /// 봇의 로그를 출력하는 함수
         /// </summary>
